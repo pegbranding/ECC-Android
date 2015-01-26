a4694ac35b5da4326fefde0bd4c15f87e8da82ee
ncolom <n924c258@ku.edu>
2015-01-26 14:57:23 -0600

    Began fleshing out Notes Activity

--- ECC_Android_App/app/src/main/java/com/example/EECS_581/ecc_android_app/ComposeNote.java
+++ ECC_Android_App/app/src/main/java/com/example/EECS_581/ecc_android_app/ComposeNote.java
@@ -8,6 +8,52 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * Created by ncolom on 1/26/15.
+ */
+public class ComposeNote extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_notes, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
+
+/*
+package com.example.EECS_581.ecc_android_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Notes extends Activity {
 
@@ -57,3 +103,5 @@ public class Notes extends Activity {
     }
 
 }
+
+ */
