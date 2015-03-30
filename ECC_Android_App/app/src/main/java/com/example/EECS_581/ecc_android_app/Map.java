d4bdc08c9683679471f2926019f55317adc164f3
gbusing <busing.g@gmail.com>
2015-03-30 14:40:50 -0500

    Adding pulling from website integration on map

--- ECC_Android_App/app/src/main/java/com/example/EECS_581/ecc_android_app/Map.java
+++ ECC_Android_App/app/src/main/java/com/example/EECS_581/ecc_android_app/Map.java
@@ -1,29 +1,36 @@
 package com.example.EECS_581.ecc_android_app;
 
-import android.gesture.Gesture;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.view.ContextMenu;
-import android.view.GestureDetector;
-import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 
 
 public class Map extends Fragment {
+    String image1path = "http://people.eecs.ku.edu/~gbusing/floor4.png";
+    String image2path = "http://people.eecs.ku.edu/~gbusing/ballroomfloor5.png";
+    String image3path = "http://people.eecs.ku.edu/~gbusing/big12floor5.png";
+    String image4path = "http://people.eecs.ku.edu/~gbusing/kansasroomfloor6.png";
+    String image5path = "http://people.eecs.ku.edu/~gbusing/malottroomfloor6.png";
 
+    private Handler mHandler = new Handler();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +43,26 @@ public class Map extends Fragment {
 
         Button nextButton = (Button)V.findViewById(R.id.nextbutton);
         Button previousButton = (Button)V.findViewById(R.id.previousbutton);
-        ImageView image1 = (ImageView)V.findViewById(R.id.floor4image);
-        ImageView image2 = (ImageView)V.findViewById(R.id.ballroomfloor5image);
-        ImageView image3 = (ImageView)V.findViewById(R.id.big12floor5image);
-        ImageView image4 = (ImageView)V.findViewById(R.id.kansasroomfloor6image);
-        ImageView image5 = (ImageView)V.findViewById(R.id.malottroomfloor6image);
+        ImageView image1view = (ImageView)V.findViewById(R.id.image1);
+        ImageView image2view = (ImageView)V.findViewById(R.id.image2);
+        ImageView image3view = (ImageView)V.findViewById(R.id.image3);
+        ImageView image4view = (ImageView)V.findViewById(R.id.image4);
+        ImageView image5view = (ImageView)V.findViewById(R.id.image5);
 
+        ImageView[] imageviewarray = {image1view, image2view, image3view, image4view, image5view};
+        String[] imagepatharray = {image1path, image2path, image3path, image4path, image5path};
 
+        DisplayImageOptions defaultoptions = new DisplayImageOptions.Builder()
+                .cacheOnDisk(true).cacheInMemory(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(V.getContext())
+                .defaultDisplayImageOptions(defaultoptions)
+                .memoryCache(new WeakMemoryCache())
+                .discCacheSize(100 * 1024 * 1024).build();
+
+        ImageLoader.getInstance().init(config);
 
         View.OnClickListener newListener = new View.OnClickListener() {
             @Override
@@ -61,7 +81,7 @@ public class Map extends Fragment {
                         int oldprogress = ((ECCFlipper.getDisplayedChild() * 100) / (ECCFlipper.getChildCount() - 1));
                         eccprogress.setProgress(oldprogress);
 
-                    break;
+                        break;
                     case R.id.previousbutton:
 
                         Toast.makeText(V.getContext(), "Previous Map", Toast.LENGTH_SHORT).show();
@@ -74,13 +94,20 @@ public class Map extends Fragment {
                         int oldprogress2 = ((ECCFlipper.getDisplayedChild() * 100) / (ECCFlipper.getChildCount() - 1));
                         eccprogress2.setProgress(oldprogress2);
 
-                    break;
+                        break;
                 }
             }
         };
 
         nextButton.setOnClickListener(newListener);
         previousButton.setOnClickListener(newListener);
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        for(int i = 0; i < imageviewarray.length; i++)
+        {
+            imageLoader.displayImage(imagepatharray[i], imageviewarray[i]);
+        }
+
         return V;
     }
 
